1520d4c4e72ac5741b218aac6259c5e57e513355
Andros Fenollosa <andros@fenollosa.email>
2023-10-13 16:22:26 +0200

    Add avif

--- place-images.fish
+++ place-images.fish
@@ -9,7 +9,7 @@ set FILENAME_OUTPUT "pack-place-images.zip"
 rm -rf $PATH_TEMP $FILENAME_OUTPUT
 
 # Make folder
-mkdir -p $PATH_TEMP/jpg $PATH_TEMP/webp
+mkdir -p $PATH_TEMP/jpg $PATH_TEMP/webp $PATH_TEMP/avif
 
 # Download random images
 for i in (seq 1 10)
@@ -17,6 +17,8 @@ for i in (seq 1 10)
     curl -L "{$SOURCE_DOWNLOAD_IMAGES}?sig=$i" -o $PATH_TEMP/jpg/$FILENAME_IMAGE.jpg
     # Transform jpg to webp
     cwebp $PATH_TEMP/jpg/$FILENAME_IMAGE.jpg -o $PATH_TEMP/webp/$FILENAME_IMAGE.webp
+    # Transform jpg to avif
+    avifenc $PATH_TEMP/jpg/$FILENAME_IMAGE.jpg -o $PATH_TEMP/avif/$FILENAME_IMAGE.avif
 end
 
 # Download square images 800x800
@@ -25,6 +27,8 @@ for j in (seq 1 10)
     curl -L "{$SOURCE_DOWNLOAD_IMAGES}800x800/?sig=$j" -o $PATH_TEMP/jpg/$FILENAME_IMAGE.jpg
     # Transform jpg to webp
     cwebp $PATH_TEMP/jpg/$FILENAME_IMAGE.jpg -o $PATH_TEMP/webp/$FILENAME_IMAGE.webp
+    # Transform jpg to avif
+    avifenc $PATH_TEMP/jpg/$FILENAME_IMAGE.jpg -o $PATH_TEMP/avif/$FILENAME_IMAGE.avif
 end
 
 # Download rectangle images 800x400
@@ -33,6 +37,8 @@ for k in (seq 1 10)
     curl -L "{$SOURCE_DOWNLOAD_IMAGES}800x400/?sig=$k" -o $PATH_TEMP/jpg/$FILENAME_IMAGE.jpg
     # Transform jpg to webp
     cwebp $PATH_TEMP/jpg/$FILENAME_IMAGE.jpg -o $PATH_TEMP/webp/$FILENAME_IMAGE.webp
+    # Transform jpg to avif
+    avifenc $PATH_TEMP/jpg/$FILENAME_IMAGE.jpg -o $PATH_TEMP/avif/$FILENAME_IMAGE.avif
 end
 
 # Download people images 200x200
@@ -41,6 +47,8 @@ for l in (seq 1 10)
     curl -L "{$SOURCE_DOWNLOAD_IMAGES}200x200/?people\&sig=$l" -o $PATH_TEMP/jpg/$FILENAME_IMAGE.jpg
     # Transform jpg to webp
     cwebp $PATH_TEMP/jpg/$FILENAME_IMAGE.jpg -o $PATH_TEMP/webp/$FILENAME_IMAGE.webp
+    # Transform jpg to avif
+    avifenc $PATH_TEMP/jpg/$FILENAME_IMAGE.jpg -o $PATH_TEMP/avif/$FILENAME_IMAGE.avif
 end
 
 # Zip images
